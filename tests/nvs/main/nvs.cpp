5a0dba2b3009b1aa681724a92ddccf646f5c58d6
chegewara <imperiaonline4@gmail.com>
2021-12-16 09:15:39 +0100

    Update example

--- tests/nvs/main/nvs.cpp
+++ tests/nvs/main/nvs.cpp
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include "nvs_comp.h"
+NVS nvs;
+NVS nvs2;
 
 extern "C" void app_main(void)
 {
-    NVS nvs;
-    NVS nvs2;
-    ESP_ERROR_CHECK(nvs2.init("nvs1"));
-
     ESP_ERROR_CHECK(nvs.init());
     ESP_ERROR_CHECK(nvs.open("test"));
     ESP_ERROR_CHECK(nvs.setString("test_v", "this is test string"));
@@ -19,9 +17,17 @@ extern "C" void app_main(void)
     printf("UsedEntries = (%d), FreeEntries = (%d), AllEntries = (%d), Count = (%d)\n",
         nvs_stats.used_entries, nvs_stats.free_entries, nvs_stats.total_entries, nvs_stats.namespace_count);
 
+    nvs.close();
+    printf("\n\n");
+    ESP_ERROR_CHECK(nvs2.init("nvs1"));
+    ESP_ERROR_CHECK(nvs2.open("test"));
+    ESP_ERROR_CHECK(nvs2.setString("test_str", "this is test string on partition with label nvs1"));
+    ESP_ERROR_CHECK(nvs2.commit());
+    nvs2.dump();
+    nvs2.close();
+
     ESP_ERROR_CHECK(nvs2.getStats(&nvs_stats));
     printf("UsedEntries = (%d), FreeEntries = (%d), AllEntries = (%d), Count = (%d)\n",
         nvs_stats.used_entries, nvs_stats.free_entries, nvs_stats.total_entries, nvs_stats.namespace_count);
 
-    nvs.close();
 }
